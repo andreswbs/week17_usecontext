881c823c8e54c20c74362053af1d0db1b23a47d8
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-08-01 22:30:31 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {createContext, useState} from 'react'
+import Component5 from './Component5';
+
+export const UserContext = createContext();
+
+function Component2() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
 
 function App() {
+  const [user, setUser] = useState("Jesse Hall");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <h3>Change user name:</h3>
+      <input value={user} onChange={
+        ({target}) => { setUser(target.value) }
+      }
+      />
+      <Component2 user={user} />
+    </UserContext.Provider>
   );
 }
 
